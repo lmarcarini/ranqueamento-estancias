c0a50bf6f72a48c0ecc644ea032b1800606902e8
lmarcarini <leonardo.marcarini@yahoo.com.br>
2022-03-23 16:04:29 -0300

    modificado página inicial

--- pages/gerenciar.js
+++ pages/gerenciar.js
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+
 export default function Gerenciar() {
-  return <div>Em construção</div>;
+  const [cadastroAberto, setCadastroAberto] = useState(false);
+
+  const toogleCadastroAberto = () => {
+    setCadastroAberto(!cadastroAberto);
+  };
+
+  return (
+    <Container className="mt-3">
+      <Form>
+        <Form.Check
+          type="switch"
+          label="Permitir cadastros de informações do município"
+          id="permitir-cadastro"
+          value={cadastroAberto}
+          onClick={toogleCadastroAberto}
+          className="mb-3"
+        />
+        <Button
+          disabled={cadastroAberto}
+          tooltip="É necessário que o cadastro esteja fechado para realizar essa ação"
+        >
+          Divulgar Validação
+        </Button>
+      </Form>
+    </Container>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,28 @@ import Calendar from "../components/Calendar";
 export default function Home() {
   return (
     <Container className="mt-3">
-      <p>
+      <h4>
         Sistema de cadastro de informações para ranqueamento das estâncias
         turísticas do estado de São Paulo.
+      </h4>
+      <p>
+        O ranqueamento das estâncias turísticas é baseado em três categorias:
+      </p>
+      <ol type="A">
+        <li>Atendimento a legislação</li>
+        <li>Cumprimento de requisitos</li>
+        <li>Realização de pleitos com destinação turística</li>
+      </ol>
+      <p>
+        De acordo com os dados preenchidos e verificados pela Secretaria de
+        Turismo, será dada uma pontuação para cada município. Os municípios com
+        as menores pontuações terão o título de estância turística substituído.
       </p>
       <p>
-        O ranqueamento das estâncias turísticas é baseado em três categorias,
-        atendimento a legislação, cumprimento de requisitos e realização de
-        pleitos com destinação turística. De acordo com os dados preenchidos e
-        verificados pela Secretaria de Turismo, será dada uma pontuação para
-        cada município. Os municípios com as menores pontuações terão o título
-        de estância turística substituído.
-      </p>
-      <p>
-        Fique atento aos prazos para envio de dados e para abrimento de recurso.
+        <b>
+          Fique atento aos prazos para envio de dados e para abrimento de
+          recurso.
+        </b>
       </p>
       <Calendar />
     </Container>
